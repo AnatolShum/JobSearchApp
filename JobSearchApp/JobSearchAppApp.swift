5dcab3ca8fad88cc1c2b976e49322409b10cad5a
Anatolii Shumov <seminarsds@gmail.com>
2024-03-13 11:58:39 +0100

    initial commit

---
+++ JobSearchApp/JobSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSearchAppApp.swift
+//  JobSearchApp
+//
+//  Created by Anatolii Shumov on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct JobSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
